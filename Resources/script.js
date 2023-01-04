79ea7bf7268fb4b28be6bcb7ee2b7a3e032c3eec
Andrew Weikkola <andrewweikkola>
2023-01-04 12:03:26 -0800

    Working background images. Added html for other pages

--- Resources/script.js
+++ Resources/script.js
@@ -1,5 +1,45 @@
 
+var imageNum = 1;
+var background = document.getElementsByClassName("main");
 
 function leftScroll() {
-    
+    imageNum -= 1;
+    if (imageNum < 1) {
+        imageNum = 4;
+    }
+    else {
+
+    }
+    console.log(imageNum);
+    scroll();
+};
+
+function rightScroll(){
+    imageNum += 1;
+    if (imageNum > 4) {
+        imageNum = 1;
+    }
+    else {
+
+    }
+    console.log(imageNum);
+    scroll();
+};
+
+function scroll() {
+    switch(imageNum){
+        case 1:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(resources/images/main.jpg)"
+            break;
+        case 2:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(resources/images/hole.jpg)"
+            console.log("helllo")
+            break;
+        case 3:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(resources/images/geese.jpg)"
+            break;
+        case 4:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(resources/images/snow-louie.jpg)"
+            break;
+        }
 }
