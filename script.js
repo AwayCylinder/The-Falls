d6ccccbd7bde07c63dfe0e5011782db9d6a5015f
Andrew Weikkola <andrewweikkola>
2023-01-04 18:29:58 -0800

    update file name

---
+++ script.js
@@ -0,0 +1,70 @@
+
+var imageNum = 1;
+var background = document.getElementsByClassName("main");
+
+function leftScroll() {
+    imageNum -= 1;
+    if (imageNum < 1) {
+        imageNum = 4;
+    }
+    else {
+
+    }
+    console.log(imageNum);
+    scroll();
+};
+
+function rightScroll(){
+    imageNum += 1;
+    if (imageNum > 4) {
+        imageNum = 1;
+    }
+    else {
+
+    }
+    console.log(imageNum);
+    scroll();
+};
+
+function scroll() {
+    switch(imageNum){
+        case 1:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(Resources/images/main.jpg)"
+            break;
+        case 2:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(Resources/images/hole.jpg)"
+  
+            break;
+        case 3:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(Resources/images/geese.jpg)"
+            break;
+        case 4:
+            document.getElementsByClassName("main")[0].style.backgroundImage = "url(Resources/images/snow-louie.jpg)"
+            break;
+        }
+}
+
+window.onmousemove = function (event) {
+    if (event.clientY < 100) {
+        document.getElementsByClassName("header")[0].style.top = "0px"
+    }
+    else {
+        document.getElementsByClassName("header")[0].style.top = "-101px"
+    }
+};
+
+
+
+window.setInterval(picTimer, 5000);
+
+function picTimer() {
+    imageNum += 1;
+    if (imageNum > 4) {
+        imageNum = 1;
+
+    }
+    else {
+        console.log(imageNum);
+    }
+    scroll();
+};
